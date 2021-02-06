8cc9e03db1d27513f13bdb3c1ef8949fdf5d89c7
jinxiao <jinxiao@xxbmm.com>
2021-02-06 20:50:02 +0800

    create ecr repo only when doesn't exist

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -e
 REGISTRY="688559712485.dkr.ecr.us-west-1.amazonaws.com"
 mvn clean package
 for project in `ls | grep galaxy `;do
-  aws ecr create-repository --repository-name $project --region=us-west-1
+  aws ecr describe-repositories --repository-names ${PROJECT} || aws ecr create-repository --repository-name ${PROJECT}
   docker build -t $REGISTRY/$project --build-arg PROJECT=$project .
   docker push $REGISTRY/$project
 done
