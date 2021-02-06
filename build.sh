4ea03b5758f68006cafcd2b4d1161158f2f4c930
jinxiao <jinxiao@xxbmm.com>
2021-02-06 20:56:57 +0800

    less output for maven

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 set -e
 REGISTRY="688559712485.dkr.ecr.us-west-1.amazonaws.com"
-mvn clean package
+mvn clean package -B
 for project in `ls | grep galaxy `;do
-  aws ecr describe-repositories --repository-names ${PROJECT} || aws ecr create-repository --repository-name ${PROJECT}
+  aws ecr describe-repositories --repository-names ${project} || aws ecr create-repository --repository-name ${project}
   docker build -t $REGISTRY/$project --build-arg PROJECT=$project .
   docker push $REGISTRY/$project
 done
