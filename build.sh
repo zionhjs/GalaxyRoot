a2b1844b28095a6b00c47ec218894ecef6bb42c6
jinxiao <jinxiao@xxbmm.com>
2021-02-06 20:07:49 +0800

    try to fix aws ecr create-repository

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 REGISTRY="688559712485.dkr.ecr.us-west-1.amazonaws.com"
 mvn clean package
 for project in `ls | grep galaxy `;do
-  aws ecs create-repository --repository-name $project
+  aws ecr create-repository --repository-name $project --region=us-west-1
   docker build -t $REGISTRY/$project --build-arg PROJECT=$project .
   docker push $REGISTRY/$project
 done
