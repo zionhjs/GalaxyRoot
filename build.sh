a431cc8bff3ff56d2c5073a91897dbbad4f974c3
jinxiao <jinxiao@xxbmm.com>
2021-02-06 19:49:06 +0800

    add mvn command to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 REGISTRY="688559712485.dkr.ecr.us-west-1.amazonaws.com"
+mvn clean package
 for project in `ls | grep galaxy `;do
   docker build -t $REGISTRY/$project --build-arg PROJECT=$project .
   docker push $REGISTRY/$project
