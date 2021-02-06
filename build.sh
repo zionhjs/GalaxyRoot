b9a68c5bc755e8f7941d50996e67ab05788134a7
jinxiao <jinxiao@xxbmm.com>
2021-02-06 21:38:05 +0800

    exclude galaxy-common

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -e
 export AWS_PAGER=""
 REGISTRY="688559712485.dkr.ecr.us-west-1.amazonaws.com"
 mvn clean package -B
-for project in `ls | grep galaxy `;do
+for project in `ls | grep galaxy | grep -v galaxy-common `;do
   aws ecr describe-repositories --repository-names ${project} || aws ecr create-repository --repository-name ${project}
   docker build -t $REGISTRY/$project --build-arg PROJECT=$project .
   docker push $REGISTRY/$project
