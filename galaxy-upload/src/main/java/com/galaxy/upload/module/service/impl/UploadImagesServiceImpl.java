8afee64845c40e8fc48fda354b1e439502480892
cjm <1061076142@qq.com>
2021-02-23 11:34:01 +0800

    桶优化

--- galaxy-upload/src/main/java/com/galaxy/upload/module/service/impl/UploadImagesServiceImpl.java
+++ galaxy-upload/src/main/java/com/galaxy/upload/module/service/impl/UploadImagesServiceImpl.java
@@ -104,7 +104,7 @@ public class UploadImagesServiceImpl extends AbstractService<Images> implements 
 
             //业务状态(1普通图片:2为360°图片)
             if ("360".equals(statusName)){
-                bucketName = imageBucketName;
+                bucketName = image360BucketName;
                 images.setStatus(2);
             }else if ("interior".equals(statusName)){
                 bucketName = imageBucketName;
