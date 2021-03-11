1598b50d5d4dc42472315bd452458dbc8bbe8dc4
jiansenhuang <jiansenhuang2016@gmail.com>
2021-03-11 10:15:39 -0800

    updated aws CodePipeLine config files

---
+++ shutdown.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+stop_process_by_service_name() {
+    service_name=$1
+    if [[ x${service_name} == "x" ]]; then
+        echo "service name is empty, exit..."
+        exit 1
+    fi
+
+
+    pid=$(ps -ef | grep "${service_name}" | grep -v grep | awk '{print $2}')
+    if [[ -z ${pid} ]]; then
+        echo "service ${service_name} is not running..."
+    else
+        until [[ -z ${pid} ]]; do
+            echo "service ${service_name} will be stopped..."
+            kill "${pid}"
+            sleep 5
+            pid=$(ps -ef | grep "${service_name}" | grep -v grep | awk '{print $2}')
+        done
+
+        echo "service ${service_name} has been stopped..."
+        sleep 5
+    fi
+}
+
+stop_process_by_service_name "galaxy-upload"
+stop_process_by_service_name "galaxy-ucenter"
+stop_process_by_service_name "galaxy-cms"
+stop_process_by_service_name "galaxy-gateway"
+stop_process_by_service_name "galaxy-config"
+stop_process_by_service_name "galaxy-eureka"
---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+workspace=/home/ubuntu/galaxy-root
+cd ${workspace}
+
+start_process_by_process_name() {
+    service_name=$1
+    if [[ x${service_name} == "x" ]]; then
+        echo "service name is empty, exit..."
+        exit 1
+    fi
+
+    pid=$(ps -ef | grep "${service_name}" | grep -v grep | awk '{print $2}')
+    if [[ x${pid} != "x" ]]; then
+        echo "service ${service_name} is running, stop it first..."
+    else
+        nohup java -jar ${service_name}-1.0.0.jar >> ${workspace}/galaxy-root.log 2>&1 &
+        echo "service ${service_name} is started..."
+        sleep 5
+    fi
+}
+
+start_process_by_process_name "galaxy-eureka"
+start_process_by_process_name "galaxy-config"
+start_process_by_process_name "galaxy-gateway"
+start_process_by_process_name "galaxy-cms"
+start_process_by_process_name "galaxy-ucenter"
+start_process_by_process_name "galaxy-upload"
+start_process_by_process_name "galaxy-admin-server"
