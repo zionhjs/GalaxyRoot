d945b71c460a15d51fc938aa606899a2751b8094
jiansenhuang <jiansenhuang2016@gmail.com>
2021-03-14 19:43:43 -0700

    update

--- galaxy-upload/deploy/stop.sh
+++ galaxy-upload/deploy/stop.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-Service_name=galaxy-upload
-Docker_running=`sudo docker ps |sudo grep $Service_name`
 
-if [ -n "$Doocker_running" ];then
-     sudo docker rm -f $Service_name >/dev/null 2>1
-     sudo docker rmi -f `sudo docker images|sudo grep -v openjdk|sudo awk '{print $3}'|sudo grep -v IMAGE` >/dev/null 2>1
-else
-     sudo echo 'exit 0'
-fi
+sudo docker rm -f galaxy-upload >/dev/null 2>1
