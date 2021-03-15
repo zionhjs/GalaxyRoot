260570d0101718611420deb846423230052f8961
jiansenhuang <jiansenhuang2016@gmail.com>
2021-03-14 19:02:02 -0700

    update

--- galaxy-eureka/deploy/stop.sh
+++ galaxy-eureka/deploy/stop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-Service_name=galaxy-eureka
-Docker_running=`sudo docker ps |grep $Service_name`
+#Service_name=galaxy-eureka
+#Docker_running=`sudo docker ps |grep $Service_name`
+#
+#if [ -n "$Doocker_running" ];then
+#     sudo docker rm -f $Service_name >/dev/null 2>1
+#     sudo docker rmi -f `sudo docker images|grep -v openjdk|awk '{print $3}'|grep -v IMAGE` >/dev/null 2>1
+#else
+#     echo "exit 0"
+#fi
 
-if [ -n "$Doocker_running" ];then
-     sudo docker rm -f $Service_name >/dev/null 2>1
-     sudo docker rmi -f `sudo docker images|grep -v openjdk|awk '{print $3}'|grep -v IMAGE` >/dev/null 2>1
-else
-     exit 0
-fi
--- galaxy-ucenter/deploy/stop.sh
+++ galaxy-ucenter/deploy/stop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-Service_name=galaxy-ucenter
-Docker_running=`sudo docker ps |grep $Service_name`
-
-if [ -n "$Doocker_running" ];then
-     sudo docker rm -f $Service_name >/dev/null 2>1
-     sudo docker rmi -f `sudo docker images|grep -v openjdk|awk '{print $3}'|grep -v IMAGE` >/dev/null 2>1
-else
-     exit 0
-fi
+#Service_name=galaxy-ucenter
+#Docker_running=`sudo docker ps |grep $Service_name`
+#
+#if [ -n "$Doocker_running" ];then
+#     sudo docker rm -f $Service_name >/dev/null 2>1
+#     sudo docker rmi -f `sudo docker images|grep -v openjdk|awk '{print $3}'|grep -v IMAGE` >/dev/null 2>1
+#else
+#     exit 0
+#fi
