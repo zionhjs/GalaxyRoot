54b19ee3c7b6107bfc7934e3f846fd55807f63ea
jiansenhuang <jiansenhuang2016@gmail.com>
2021-03-14 21:00:18 -0700

    update

--- galaxy-config/deploy/stop.sh
+++ galaxy-config/deploy/stop.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-Service_name=galaxy-config
-Docker_running=`sudo docker ps |sudo grep $Service_name`
+#Service_name=galaxy-config
+#Docker_running=`sudo docker ps |sudo grep $Service_name`
 
-if [ -n "$Doocker_running" ];then
-     sudo docker rm -f $Service_name
-     sudo docker rmi -f `sudo docker images|sudo grep -v openjdk|sudo awk '{print $3}'|sudo grep -v IMAGE` >/dev/null 2>1
-else
-     sudo echo 'exit 0'
-fi
+#if [ -n "$Doocker_running" ];then
+sudo docker rm -f galaxy-config >/dev/null 2>&1
+#     sudo docker rmi -f `sudo docker images|sudo grep -v openjdk|sudo awk '{print $3}'|sudo grep -v IMAGE` >/dev/null 2>&1
+#else
+#     sudo echo 'exit 0'
+#fi
 
