5db842cbc911c22b5c7f387484b51b0c88cea3e7
jiansenhuang <jiansenhuang2016@gmail.com>
2021-03-15 01:47:47 -0700

    update

--- galaxy-gateway/deploy/stop.sh
+++ galaxy-gateway/deploy/stop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 Service_name=galaxy-gateway
-Docker_running=`sudo docker ps |sudo grep $Service_name`
+Docker_running=`sudo docker ps |sudo grep $Service_name|wc -l`
 
-if [ -n "$Doocker_running" ];then
+if [ -n "$Docker_running" ];then
      sudo docker rm -f $Service_name >/dev/null 2>1
+     sudo docker rm -f `sudo docker ps -qa` >/dev/null 2>1
      sudo docker rmi -f `sudo docker images|sudo grep -v openjdk|sudo awk '{print $3}'|sudo grep -v IMAGE` >/dev/null 2>1
 else
      sudo echo 'exit 0'
