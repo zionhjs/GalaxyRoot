841f5d2ccdb095f84245a32d88fed6d237f4c758
jiansenhuang <jiansenhuang2016@gmail.com>
2021-03-15 22:50:21 -0700

    update

--- galaxy-admin-server/deploy/start.sh
+++ galaxy-admin-server/deploy/start.sh
@@ -2,4 +2,4 @@
 
 sleep 210
 
-sudo docker run -p 9700:9700 -d --name galaxy-admin-server 688559712485.dkr.ecr.us-west-1.amazonaws.com/galaxy-admin-server:latest
+sudo docker run -d --network host --name galaxy-admin-server 688559712485.dkr.ecr.us-west-1.amazonaws.com/galaxy-admin-server:latest
--- galaxy-cms/deploy/start.sh
+++ galaxy-cms/deploy/start.sh
@@ -2,4 +2,4 @@
 
 sleep 210
 
-sudo docker run -p 9900:9900 -d --name galaxy-cms 688559712485.dkr.ecr.us-west-1.amazonaws.com/galaxy-cms:latest
+sudo docker run -d --network host --name galaxy-cms 688559712485.dkr.ecr.us-west-1.amazonaws.com/galaxy-cms:latest
