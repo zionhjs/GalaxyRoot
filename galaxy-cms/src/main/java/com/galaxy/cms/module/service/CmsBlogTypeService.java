3e9ce77c1e45e327cf167f984f022f6a960229ba
Administrator <1061076142@qq.com>
2021-05-09 15:50:57 +0800

    增加博客管理模块

---
+++ galaxy-cms/src/main/java/com/galaxy/cms/module/service/CmsBlogTypeService.java
@@ -0,0 +1,12 @@
+package com.galaxy.cms.module.service;
+
+import com.galaxy.cms.module.model.BlogType;
+import com.galaxy.common.core.service.Service;
+
+
+/**
+* Created by CodeGenerator on 2021/05/09.
+*/
+public interface CmsBlogTypeService extends Service<BlogType> {
+
+}
